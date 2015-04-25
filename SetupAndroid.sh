a104dcb33f3f8b76dca5c27a2f91a8122bb6651b
King <a1307468@126.com>
2015-04-25 08:25:24 +0800

    Android environment Setup for Centos 6.5 64bit

---
+++ SetupAndroid.sh
@@ -0,0 +1,8 @@
+#########################################################################
+# File Name: SetupAndroid.sh
+# Author: jinlx
+# mail: a1307468@126.com
+# Created Time: 2015年04月25日 星期六 08时23分45秒
+#########################################################################
+#!/bin/bash
+yum install flex bison* gperf make gcc gcc-c++ kernel-devel zlib zlib-devel zlib.i686 zlib-devel .i686 glibc* glibc*.i686 ncurses* ncurses*.i686 libX11 libX11-devel libXext libX11.i686 libX11-devel.i686 libXext.i686 readline readline.i686 mesa-libGL* mesa-libGL*.i686 unix2dos dos2unix python-markdown* libxslt* libxslt*.i686 libtool* libtool*.i686 readline* readline*.i686 -y
